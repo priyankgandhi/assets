b591f8f8e869cfeae62d9561293629accc553a42
Priyank Gandhi <priyankgandhi@gmail.com>
2019-05-04 10:02:48 -0700

    js

---
+++ js/homepage.js
@@ -0,0 +1,340 @@
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var MasterForm = function (_React$Component) {
+  _inherits(MasterForm, _React$Component);
+
+  function MasterForm(props) {
+    _classCallCheck(this, MasterForm);
+
+    var _this = _possibleConstructorReturn(this, (MasterForm.__proto__ || Object.getPrototypeOf(MasterForm)).call(this, props));
+
+    _this.handleChange = function (event) {
+      var _event$target = event.target,
+          name = _event$target.name,
+          value = _event$target.value;
+
+      _this.setState(_defineProperty({}, name, value));
+    };
+
+    _this.handleSubmit = function (event) {
+      event.preventDefault();
+      var _this$state = _this.state,
+          zip = _this$state.zip,
+          address = _this$state.address,
+          email = _this$state.email,
+          frequency = _this$state.frequency,
+          yardsize = _this$state.yardsize,
+          username = _this$state.username,
+          password = _this$state.password;
+
+      var data = _this.state;
+      alert('Your registration detail: \n\n           Zip: ' + zip + ' \n\n           Address: ' + address + ' \n\n           frequency: ' + frequency + ' \n\n           yardsize: ' + yardsize + ' \n\n           email: ' + email + ' \n\n           password: ' + password);
+      fetch('/store/ordersubmit', {
+        method: 'POST',
+        body: data
+      });
+    };
+
+    _this._next = function () {
+      var currentStep = _this.state.currentStep;
+      currentStep = currentStep >= 2 ? 3 : currentStep + 1;
+      _this.setState({
+        currentStep: currentStep
+      });
+    };
+
+    _this._prev = function () {
+      var currentStep = _this.state.currentStep;
+      currentStep = currentStep <= 1 ? 1 : currentStep - 1;
+      _this.setState({
+        currentStep: currentStep
+      });
+    };
+
+    _this.state = {
+      currentStep: 1,
+      email: '',
+      zip: '',
+      service: '',
+      frequency: '',
+      yardsize: ''
+    };
+    return _this;
+  }
+
+  _createClass(MasterForm, [{
+    key: 'previousButton',
+
+
+    /*
+    * the functions for our button
+    */
+    value: function previousButton() {
+      var currentStep = this.state.currentStep;
+      if (currentStep !== 1) {
+        return React.createElement(
+          'button',
+          {
+            className: 'btn btn-secondary float-left',
+            type: 'button', onClick: this._prev },
+          'Back'
+        );
+      }
+      return null;
+    }
+  }, {
+    key: 'nextButton',
+    value: function nextButton() {
+      var currentStep = this.state.currentStep;
+      if (currentStep == 1) {
+        return React.createElement(
+          'button',
+          {
+            className: 'btn btn-primary float-right',
+            type: 'button', onClick: this._next },
+          'Get started'
+        );
+      } else if (currentStep == 2) {
+        return React.createElement(
+          'button',
+          {
+            className: 'btn btn-primary float-right',
+            type: 'button', onClick: this._next },
+          'Next'
+        );
+      }
+
+      return null;
+    }
+  }, {
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        React.Fragment,
+        null,
+        React.createElement(
+          'form',
+          { onSubmit: this.handleSubmit },
+          React.createElement(Step1, {
+            currentStep: this.state.currentStep,
+            handleChange: this.handleChange,
+            email: this.state.email
+          }),
+          React.createElement(Step2, {
+            currentStep: this.state.currentStep,
+            handleChange: this.handleChange,
+            username: this.state.username
+          }),
+          React.createElement(Step3, {
+            currentStep: this.state.currentStep,
+            handleChange: this.handleChange,
+            password: this.state.password
+          }),
+          this.previousButton(),
+          this.nextButton()
+        )
+      );
+    }
+  }]);
+
+  return MasterForm;
+}(React.Component);
+
+function Step1(props) {
+  if (props.currentStep !== 1) {
+    return null;
+  }
+  return React.createElement(
+    'div',
+    { className: 'row align-items-center text-center text-md-left' },
+    React.createElement(
+      'div',
+      { className: '' },
+      React.createElement(
+        'h1',
+        null,
+        'Smart Yard Maintenance'
+      ),
+      React.createElement(
+        'h2',
+        null,
+        'An affordable, quality gardening service that you can manage online '
+      ),
+      React.createElement('br', null),
+      React.createElement(
+        'div',
+        { className: 'form-inline' },
+        React.createElement(
+          'label',
+          { htmlFor: 'zip' },
+          'My home is located at \xA0\xA0'
+        ),
+        React.createElement('input', { className: 'form-control', id: 'zip', name: 'zip', type: 'text', placeholder: 'Enter Your Zip', value: props.zip, onChange: props.handleChange })
+      )
+    ),
+    React.createElement(
+      'div',
+      { className: 'col-md-6 col-lg-7 col-xl-6 offset-xl-1' },
+      React.createElement('img', { className: 'img-fluid', src: 'img/home/hero-img.png', alt: '' })
+    )
+  );
+}
+
+function Step2(props) {
+  if (props.currentStep !== 2) {
+    return null;
+  }
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      'h2',
+      null,
+      'Tell us about your maintenance needs'
+    ),
+    React.createElement(
+      'div',
+      { className: 'form-group col-xs-4' },
+      React.createElement(
+        'label',
+        { htmlFor: 'address' },
+        'Your address'
+      ),
+      React.createElement('input', { className: 'form-control', id: 'address', name: 'address', value: props.address, onChange: props.handleChange })
+    ),
+    React.createElement(
+      'div',
+      { className: 'form-group col-xs-2' },
+      React.createElement(
+        'label',
+        { htmlFor: 'yardsize' },
+        'Yard Size'
+      ),
+      React.createElement(
+        'select',
+        { className: 'custom-select form-control', id: 'yardsize', name: 'yardsize', value: props.yardsize, onChange: props.handleChange },
+        React.createElement(
+          'option',
+          null,
+          'Open this select menu'
+        ),
+        React.createElement(
+          'option',
+          { value: '1' },
+          'Under 1000 sqft'
+        ),
+        React.createElement(
+          'option',
+          { value: '2' },
+          '100-2500 sqft'
+        ),
+        React.createElement(
+          'option',
+          { value: '3' },
+          '2500+ sqft'
+        )
+      )
+    ),
+    React.createElement(
+      'div',
+      { className: 'form-group' },
+      React.createElement(
+        'label',
+        { htmlFor: 'frequency' },
+        'Service Frequency'
+      ),
+      React.createElement(
+        'div',
+        { className: 'custom-control custom-radio' },
+        React.createElement('input', { type: 'radio', id: 'customRadio1', name: 'frequency', className: 'custom-control-input form-control', value: props.frequency, onChange: props.handleChange }),
+        React.createElement(
+          'label',
+          { className: 'custom-control-label', htmlFor: 'customRadio1' },
+          'Weekly'
+        )
+      ),
+      React.createElement(
+        'div',
+        { className: 'custom-control custom-radio' },
+        React.createElement('input', { type: 'radio', id: 'customRadio2', name: 'frequency', className: 'custom-control-input form-control', value: props.frequency, onChange: props.handleChange }),
+        React.createElement(
+          'label',
+          { className: 'custom-control-label', htmlFor: 'customRadio2' },
+          'Bi-weekly'
+        )
+      ),
+      React.createElement(
+        'div',
+        { className: 'custom-control custom-radio' },
+        React.createElement('input', { type: 'radio', id: 'customRadio2', name: 'frequency', className: 'custom-control-input form-control', value: props.frequency, onChange: props.handleChange }),
+        React.createElement(
+          'label',
+          { className: 'custom-control-label', htmlFor: 'customRadio2' },
+          'Monthly'
+        )
+      )
+    )
+  );
+}
+
+function Step3(props) {
+  if (props.currentStep !== 3) {
+    return null;
+  }
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      'h2',
+      null,
+      'Last step'
+    ),
+    React.createElement(
+      'div',
+      { className: 'form-group' },
+      React.createElement(
+        'label',
+        { htmlFor: 'email' },
+        'Email'
+      ),
+      React.createElement('input', { className: 'form-control', id: 'email', name: 'email', type: 'text', placeholder: 'Email', value: props.email, onChange: props.handleChange })
+    ),
+    React.createElement(
+      'div',
+      { className: 'form-group' },
+      React.createElement(
+        'label',
+        { htmlFor: 'password' },
+        'Password'
+      ),
+      React.createElement('input', { className: 'form-control', id: 'password', name: 'password', type: 'password', placeholder: 'Password', value: props.password, onChange: props.handleChange })
+    ),
+    React.createElement(
+      'button',
+      { className: 'btn btn-success' },
+      'Sign up'
+    )
+  );
+}
+
+ReactDOM.render(React.createElement(MasterForm, null), document.getElementById('root'));
+
+// <div className="container">
+//   <ul className="list-unstyled">
+//     <li className="col-xs-6 col-sm-4 col-md-3">Mowing, edging, trimming your lawn</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Weeding</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Hedge trimming and pruning;</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Yard cleanup</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Leaf Removal</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Aenean sit amet erat nunc</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Eget porttitor lorem</li>
+//   </ul>
+// </div>
---
+++ js/homepage.jsx
@@ -0,0 +1,219 @@
+class MasterForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentStep: 1,
+      email:  '',
+      zip: '',
+      service: '',
+      frequency: '',
+      yardsize: ''
+    }
+  }
+
+  handleChange = event => {
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { zip, address, email, frequency, yardsize, username, password } = this.state
+    const data = this.state
+    alert(`Your registration detail: \n
+           Zip: ${zip} \n
+           Address: ${address} \n
+           frequency: ${frequency} \n
+           yardsize: ${yardsize} \n
+           email: ${email} \n
+           password: ${password}`)
+     fetch('/store/ordersubmit', {
+       method: 'POST',
+       body: data,
+     });
+  }
+
+  _next = () => {
+    let currentStep = this.state.currentStep
+    currentStep = currentStep >= 2? 3: currentStep + 1
+    this.setState({
+      currentStep: currentStep
+    })
+  }
+
+  _prev = () => {
+    let currentStep = this.state.currentStep
+    currentStep = currentStep <= 1? 1: currentStep - 1
+    this.setState({
+      currentStep: currentStep
+    })
+  }
+
+  /*
+  * the functions for our button
+  */
+  previousButton() {
+    let currentStep = this.state.currentStep;
+    if(currentStep !==1){
+      return (
+        <button
+          className="btn btn-secondary float-left"
+          type="button" onClick={this._prev}>
+        Back
+        </button>
+      )
+    }
+    return null;
+  }
+
+  nextButton(){
+    let currentStep = this.state.currentStep;
+    if(currentStep ==1){
+      return (
+        <button
+          className="btn btn-primary float-right"
+          type="button" onClick={this._next}>
+        Get started
+        </button>
+      )
+    } else if (currentStep == 2) {
+      return (
+        <button
+          className="btn btn-primary float-right"
+          type="button" onClick={this._next}>
+        Next
+        </button>
+      )
+    }
+
+    return null;
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+      <form onSubmit={this.handleSubmit}>
+      {/*
+        render the form steps and pass required props in
+      */}
+        <Step1
+          currentStep={this.state.currentStep}
+          handleChange={this.handleChange}
+          email={this.state.email}
+        />
+        <Step2
+          currentStep={this.state.currentStep}
+          handleChange={this.handleChange}
+          username={this.state.username}
+        />
+        <Step3
+          currentStep={this.state.currentStep}
+          handleChange={this.handleChange}
+          password={this.state.password}
+        />
+        {this.previousButton()}
+        {this.nextButton()}
+
+      </form>
+      </React.Fragment>
+    );
+  }
+}
+
+function Step1(props) {
+  if (props.currentStep !== 1) {
+    return null
+  }
+  return(
+    <div className="row align-items-center text-center text-md-left">
+       <div className="">
+         <h1>Smart Yard Maintenance</h1>
+         <h2>An affordable, quality gardening service that you can manage online </h2>
+         <br/>
+         <div className="form-inline">
+           <label htmlFor="zip">My home is located at &nbsp;&nbsp;</label>
+           <input className="form-control" id="zip" name="zip" type="text" placeholder="Enter Your Zip" value={props.zip} onChange={props.handleChange}/>
+         </div>
+       </div>
+       <div className="col-md-6 col-lg-7 col-xl-6 offset-xl-1">
+         <img className="img-fluid" src="img/home/hero-img.png" alt=""/>
+       </div>
+   </div>
+  );
+}
+
+function Step2(props) {
+  if (props.currentStep !== 2) {
+    return null
+  }
+  return(<React.Fragment>
+    <h2>Tell us about your maintenance needs</h2>
+    <div className="form-group col-xs-4">
+      <label htmlFor="address">Your address</label>
+      <input className="form-control" id="address" name="address" value={props.address} onChange={props.handleChange}/>
+    </div>
+    <div className="form-group col-xs-2">
+      <label htmlFor="yardsize">Yard Size</label>
+      <select className="custom-select form-control" id="yardsize" name="yardsize" value={props.yardsize} onChange={props.handleChange}>
+        <option>Open this select menu</option>
+        <option value="1">Under 1000 sqft</option>
+        <option value="2">100-2500 sqft</option>
+        <option value="3">2500+ sqft</option>
+      </select>
+
+    </div>
+    <div className="form-group">
+      <label htmlFor="frequency">Service Frequency</label>
+      <div className="custom-control custom-radio">
+        <input type="radio" id="customRadio1" name="frequency" className="custom-control-input form-control" value={props.frequency} onChange={props.handleChange} />
+        <label className="custom-control-label" htmlFor="customRadio1">Weekly</label>
+      </div>
+      <div className="custom-control custom-radio">
+        <input type="radio" id="customRadio2" name="frequency" className="custom-control-input form-control" value={props.frequency} onChange={props.handleChange} />
+        <label className="custom-control-label" htmlFor="customRadio2">Bi-weekly</label>
+      </div>
+      <div className="custom-control custom-radio">
+        <input type="radio" id="customRadio2" name="frequency" className="custom-control-input form-control" value={props.frequency} onChange={props.handleChange} />
+        <label className="custom-control-label" htmlFor="customRadio2">Monthly</label>
+      </div>
+
+    </div>
+    </React.Fragment>
+  );
+}
+
+function Step3(props) {
+  if (props.currentStep !== 3) {
+    return null
+  }
+  return(
+    <React.Fragment>
+    <h2>Last step</h2>
+    <div className="form-group">
+      <label htmlFor="email">Email</label>
+      <input className="form-control" id="email" name="email" type="text" placeholder="Email" value={props.email} onChange={props.handleChange} />
+    </div>
+    <div className="form-group">
+      <label htmlFor="password">Password</label>
+      <input className="form-control" id="password" name="password" type="password" placeholder="Password" value={props.password} onChange={props.handleChange} />
+    </div>
+    <button className="btn btn-success">Sign up</button>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<MasterForm />, document.getElementById('root'))
+
+// <div className="container">
+//   <ul className="list-unstyled">
+//     <li className="col-xs-6 col-sm-4 col-md-3">Mowing, edging, trimming your lawn</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Weeding</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Hedge trimming and pruning;</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Yard cleanup</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Leaf Removal</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Aenean sit amet erat nunc</li>
+//     <li className="col-xs-6 col-sm-4 col-md-3">Eget porttitor lorem</li>
+//   </ul>
+// </div>
